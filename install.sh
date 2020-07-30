7aba87b102b8ca17f6b5a56eb4241f384471936f
Mike Nye <mike.nye@gmail.com>
2020-07-30 15:54:42 +0800

    Fix errors introduced by linting

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ if [[ -n "$packages" ]]
 then
     echo "Installing required packages: $packages"
     apt-get update || true
-    apt-get install -y "$packages" || true
+    apt-get install -y $packages || true
     hash -r || true
     while read -r -d '/' CMD PKG
     do
