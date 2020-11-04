79d99bce268409d75fcebc4f7849a3082e6332d4
Matthias Wirth <matthias.wirth@gmail.com>
2020-11-04 22:05:06 +0100

    flag column

--- html/script.js
+++ html/script.js
@@ -2105,10 +2105,10 @@ function refreshFeatures() {
         value: function(plane) { return plane.icao; },
         td: '<td class="icaoCodeColumn"></td>' };
     cols.flag = {
-        text: 'Flag',
+        text: '',
         sort: function () { sortBy('country', compareAlpha, function(x) { return x.icaorange.country; }); },
-        value: function(plane) { return (plane.icaorange.flag_image ? ('<img src="' + FlagPath + plane.icaorange.flag_image + '" title="' + plane.icaorange.country + '"></img>') : ''); },
-        hStyle: 'style="width: 20px"',
+        value: function(plane) { return (plane.icaorange.flag_image ? ('<img class="flag" src="' + FlagPath + plane.icaorange.flag_image + '" title="' + plane.icaorange.country + '"></img>') : ''); },
+        hStyle: 'style="width: 20px; padding: 3px;"',
         html: true,
     };
     cols.flight = {
