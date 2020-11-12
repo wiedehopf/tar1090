e823d85c552c4438fd13dd356a4bad9df8b0b63b
Matthias Wirth <matthias.wirth@gmail.com>
2020-11-12 20:09:36 +0100

    add KML layer for testing

--- html/layers.js
+++ html/layers.js
@@ -210,6 +210,19 @@ function createBaseLayers() {
         maxZoom: 14,
     }));
 
+    world.push(new ol.layer.Vector({
+        source: new ol.source.Vector({
+            url: 'tfrs.kml',
+            format: new ol.format.KML(),
+        }),
+        name: 'tfr',
+        title: 'TFRs',
+        type: 'overlay',
+        opacity: 0.7,
+        visible: false,
+        zIndex: 99,
+    }));
+
     let nexrad = new ol.layer.Tile({
         name: 'nexrad',
         title: 'NEXRAD',
