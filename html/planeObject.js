61f0ca3e0e34a3f87332d2717079b13f17d0762b
Matthias Wirth <matthias.wirth@gmail.com>
2020-11-18 15:17:16 +0100

    fix monochromeMarkers

--- html/planeObject.js
+++ html/planeObject.js
@@ -572,12 +572,13 @@ PlaneObject.prototype.getDataSource = function() {
     // TODO: add support for Mode A/C
 };
 
-PlaneObject.prototype.getMarkerColor = function() {
+PlaneObject.prototype.getMarkerColor = function(options) {
+    options |= {}
     if (monochromeMarkers) {
-        return monochromeMarkers;
+        return hexToHSL(monochromeMarkers);
     }
 
-    let alt = this.alt_rounded;
+    let alt = options.noRound ? this.altitude : this.alt_rounded;
     if (this.category == 'C3' || this.icaoType == 'TWR' || (this.icaoType == null && this.squawk == 7777))
         alt = 'ground';
 
@@ -687,7 +688,7 @@ function altitudeColor(altitude) {
 }
 
 PlaneObject.prototype.setMarkerRgb = function() {
-    let hsl = this.getMarkerColor();
+    let hsl = this.getMarkerColor({noRound: true});
     let rgb = hslToRgb(hsl, 'array');
     if (this.shape && this.shape.svg)
         rgb = [255, 255, 255];
@@ -1908,6 +1909,37 @@ PlaneObject.prototype.drawRedDot = function(bad_position) {
     let lineFeat = new ol.Feature(geom);
     lineFeat.setStyle(this.dataSource == "mlat" ? badLineMlat : badLine);
     this.trail_features.addFeature(lineFeat);
+};
+
+function hexToHSL(hex) {
+    let r = +('0x'+ hex[1] + hex[2]) / 255;
+    let g = +('0x'+ hex[3] + hex[4]) / 255;
+    let b = +('0x'+ hex[5] + hex[6]) / 255;
+    let cmin = Math.min(r,g,b);
+    let cmax = Math.max(r,g,b);
+    let delta = cmax - cmin;
+    let h = 0, s = 0, l = 0;
+    if (delta == 0)
+        h = 0;
+    else if (cmax == r)
+        h = ((g - b) / delta) % 6;
+    else if (cmax == g)
+        h = (b - r) / delta + 2;
+    else
+        h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+
+    if (h < 0)
+        h += 360;
+
+    l = (cmax + cmin) / 2;
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+    s *= 100;
+    l *= 100;
+
+    return [h, s, l];
 };
 
 /**
