53cfad893920ec2cc4d05b2df98cddbae47715f8
Matthias Wirth <matthias.wirth@gmail.com>
2020-11-30 22:27:32 +0100

    concat example and anchors for messing with the html

---
+++ concat-example.sh
@@ -0,0 +1,23 @@
+function magic() {
+    FN="$1.$2"
+    MD5="$1_$(md5sum "$FN" | cut -f1 -d' ').$2"
+    sed -i -e "s/$FN/$MD5/" index.html
+    mv "$FN" "$MD5"
+}
+
+
+for file in $(grep -oP -e '"stylesheet" href="\K[^"]*' index.html); do
+    sed -i -e "\\#$file#d" index.html
+    cat $file >> all.css
+done
+sed -i -e 's$.*CSS_ANCHOR.*$\0\n<link rel="stylesheet" href="all.css" type="text/css" />$' index.html
+magic all css
+
+
+for file in $(grep -oP -e 'script src="\K[^"]*' index.html); do
+    sed -i -e "\\#$file#d" index.html
+    cat "$file" >> all.js
+done
+sed -i -e 's$.*JS_ANCHOR.*$\0\n<link rel="stylesheet" href="all.css" type="text/css" />$' index.html
+magic all js
+
