464910903f9b75c11b2dfe0fa92e8a581ded6685
Matthias Wirth <matthias.wirth@gmail.com>
2022-02-10 15:27:00 +0100

    don't show bogus geom_alt in labels when geom_alt not available

--- html/script.js
+++ html/script.js
@@ -7363,6 +7363,9 @@ function loadEGM() {
 }
 function adjust_geom_alt(alt, pos) {
     if (geomUseEGM && egmLoaded) {
+        if (alt == null) {
+            return alt;
+        }
         return egm96.ellipsoidToEgm96(pos[1], pos[0], alt * 0.3048) / 0.3048;
     } else {
         return alt;
