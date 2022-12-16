9cbbe46ef705c7c86b2fb9a4cfd05467963f2646
Mark Pentler <27921983+mpentler@users.noreply.github.com>
2022-12-16 23:40:58 +0000

    Update layers.js
    Adding line (with credit comment) to pull in the German AWACS orbit areas

--- html/layers.js
+++ html/layers.js
@@ -555,6 +555,9 @@ function createBaseLayers() {
         });
     };
 
+    // Taken from https://github.com/olithissen/AwacsOrbitsDE
+    europe.push(createGeoJsonLayer('DE AWACS Orbits', 'deawacsorbits', 'geojson/DE_Mil_AWACS_Orbits.geojson', 'rgba(252, 186, 3, 0.3)', 'rgba(252, 186, 3, 1)', false));
+    
     // Taken from https://github.com/alkissack/Dump1090-OpenLayers3-html
     europe.push(createGeoJsonLayer('UK Radar Corridors', 'ukradarcorridors', 'geojson/UK_Mil_RC.geojson', 'rgba(22, 171, 22, 0.3)', 'rgba(22, 171, 22, 1)'));
     europe.push(createGeoJsonLayer('UK A2A Refueling', 'uka2arefueling', 'geojson/UK_Mil_AAR_Zones.geojson', 'rgba(52, 50, 168, 0.3)', 'rgba(52, 50, 168, 1)'));
