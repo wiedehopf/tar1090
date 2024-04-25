64306b7da1a72356ca7e04a80a0c31edd0747544
Matthias Wirth <matthias.wirth@gmail.com>
2024-04-25 16:18:20 +0200

    install.sh minor quoting changes

--- install.sh
+++ install.sh
@@ -120,7 +120,7 @@ function revision() {
 
 if ! { [[ "$1" == "test" ]] && cd "$gpath/git-db"; }; then
     DB_VERSION_NEW=$(curl --silent --show-error "https://raw.githubusercontent.com/wiedehopf/tar1090-db/master/version")
-    if  [[ "$(cat $gpath/git-db/version 2>/dev/null)" != "$DB_VERSION_NEW" ]]; then
+    if  [[ "$(cat "$gpath/git-db/version" 2>/dev/null)" != "$DB_VERSION_NEW" ]]; then
         getGIT "$db_repo" "master" "$gpath/git-db" || true
     fi
 fi
@@ -147,7 +147,7 @@ if [[ "$1" == "test" ]] || [[ -n "$git_source" ]]; then
     TAR_VERSION="$(date +%s)_${RANDOM}${RANDOM}"
 else
     VERSION_NEW=$(curl --silent --show-error "https://raw.githubusercontent.com/wiedehopf/tar1090/master/version")
-    if  [[ "$(cat $gpath/git/version 2>/dev/null)" != "$VERSION_NEW" ]]; then
+    if  [[ "$(cat "$gpath/git/version" 2>/dev/null)" != "$VERSION_NEW" ]]; then
         if ! getGIT "$repo" "master" "$gpath/git"; then
             echo "Unable to download files, exiting! (Maybe try again?)"
             exit 1
