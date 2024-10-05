f65d62178e5dcf28a33fac0d98b871656fbc3b98
Matthias Wirth <matthias.wirth@gmail.com>
2024-10-05 19:37:54 +0200

    fix lastRenderExtent not initialized on startup with noIsolation
    fixes: https://github.com/wiedehopf/tar1090/issues/340

--- html/script.js
+++ html/script.js
@@ -5878,6 +5878,8 @@ function processURLParams(){
         }
     }
 
+    lastRenderExtent = getRenderExtent();
+
     if (urlIcaos.length > 0) {
         const icaos = urlIcaos;
         if (!usp.has('noIsolation'))
