b874ae4edb7a5ed161340e2fa29d5f001e316782
Matthias Wirth <matthias.wirth@gmail.com>
2024-11-09 06:42:20 +0100

    avoid error on exit

--- tar1090.sh
+++ tar1090.sh
@@ -2,7 +2,7 @@
 
 set -e
 trap 'echo "[ERROR] Error in line $LINENO when executing: $BASH_COMMAND"' ERR
-trap 'echo tar1090.sh: exiting; trap - SIGTERM; kill -- -$( ps opgid= $$ | tr -d " " ) || true; exit 0' SIGTERM SIGINT SIGHUP SIGQUIT
+trap 'echo tar1090.sh: exiting; trap - SIGTERM; pkill -P $$ || true; exit 0' SIGTERM SIGINT SIGHUP SIGQUIT
 
 # run with lowest priority
 renice 20 $$ || true
