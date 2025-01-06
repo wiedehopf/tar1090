694683486112f3a7145f863dbdc487cd1d9787c6
Matthias Wirth <matthias.wirth@gmail.com>
2025-01-06 19:46:14 +0000

    fix bad var reference

--- html/script.js
+++ html/script.js
@@ -6832,7 +6832,7 @@ function initSitePos() {
             if (SitePosition) {
                 TAR.planeMan.cols.sitedist.sort();
             } else {
-                planeMan.sortAscending = false;
+                TAR.planeMan.sortAscending = false;
                 TAR.planeMan.cols.altitude.sort();
             }
         }
