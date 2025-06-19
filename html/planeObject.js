12fbb2503bb7122f36190a6ac734ebb58d9ec98c
Matthias Wirth <matthias.wirth@gmail.com>
2025-06-19 08:34:13 +0000

    better debug for null in route response

--- html/planeObject.js
+++ html/planeObject.js
@@ -2926,6 +2926,7 @@ function routeDoLookup(currentTime) {
                 for (var route of routes) {
                     if (!route) {
                         console.error(`Route API returned this invalid element in the array ${route}`);
+                        console.log(routes);
                         continue;
                     }
                     // let's log just a little bit of what's happening
