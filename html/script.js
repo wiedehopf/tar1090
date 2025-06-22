e1c623e1668ae0cae518f52f9ac0a23bbd67872d
TEBandCo <47662568+TEBandCo@users.noreply.github.com>
2025-06-22 16:19:18 +0100

    Add toggle for IATA vs ICAO airport codes

--- html/script.js
+++ html/script.js
@@ -1659,6 +1659,16 @@ jQuery('#selected_altitude_geom1')
                 }
             }
         });
+
+        new Toggle({
+            key: "useIataAirportCodes",
+            display: "Show IATA airport codes",
+            container: "#settingsRight",
+            init: useIataAirportCodes,
+            setState: function(state) {
+                useIataAirportCodes = state;
+            }
+        });
     } else {
         useRouteAPI = false;
     }
