7d04685d7e6fe57ff372dc03253d4c897073a927
Maria Merkel <130244852+MariaMerkel@users.noreply.github.com>
2025-08-30 11:23:01 +0200

    Update database version source in install.sh

--- install.sh
+++ install.sh
@@ -119,7 +119,7 @@ function revision() {
 }
 
 if ! { [[ "$1" == "test" ]] && cd "$gpath/git-db"; }; then
-    DB_VERSION_NEW=$(curl --silent --show-error "https://raw.githubusercontent.com/wiedehopf/tar1090-db/master/version")
+    DB_VERSION_NEW=$(curl --silent --show-error "https://raw.githubusercontent.com/redesigned-train/tar1090-db/master/version")
     if  [[ "$(cat "$gpath/git-db/version" 2>/dev/null)" != "$DB_VERSION_NEW" ]]; then
         getGIT "$db_repo" "master" "$gpath/git-db" || true
     fi
